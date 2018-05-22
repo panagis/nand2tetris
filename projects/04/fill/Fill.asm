f182091146998fecf54d67c411bad4492ec9d8cb
Panagis Artoumas <panagisart@yahoo.com>
2018-05-22 23:48:31 +0100

    Implement Mult and Fill programs in assembly
    Implement the two programs (Fill and Mult) presented in the chapter 4 of the course's book, in the assembly language.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,61 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(READ_KEYBOARD)
+    // if (last filled color == white) go to @IS_WHITE
+    // else go to @IS_BLACK
+    @fillColor
+    D=M
+    @IS_WHITE
+    D;JEQ
+
+    // if (key was pressed) go to @READ_KEYBOARD *** because the screen is already black
+    // else go to @FILL_SCREEN
+    (IS_BLACK)
+    @KBD
+    D=M    
+    @READ_KEYBOARD
+    D;JNE
+    @fillColor
+    M=0
+    @FILL_SCREEN
+    0;JMP
+
+    // if (key was not pressed) go to @READ_KEYBOARD *** because the screen is already white
+    // else go to @FILSCREN
+    (IS_WHITE)
+    @KBD
+    D=M
+    @READ_KEYBOARD
+    D;JEQ    
+    @fillColor
+    M=-1 // -1 in decimal = 1111 1111 1111 1111 in binary in a 16-bit register
+    @FILL_SCREEN
+    0;JMP
+
+// if (@fillColor == 0) screen becomes white
+// else if (@fillColor == -1) screen becomes black
+(FILL_SCREEN)
+    @SCREEN
+    D=A  //  D = base address of the SCREEN's address space
+    @i   // @i = a pointer to the base address of the SCREEN's address space
+    M=D
+
+    (LOOP)
+        @fillColor
+        D=M
+        @i
+        A=M // dereference the @i pointer
+        M=D // set the value of the @fillColor variable into the SCREEN's register
+        @i
+        M = M + 1 // change the @i pointer to point to the next memory location (next RAM register)
+        D=M
+
+        @24576 // 24576 is the address of the last register of the SCREEN's address space
+               // screen base + screen size = 16384 + 256x512bits/16 = 24576
+        D=A-D  // D = 24576 - value (which is an address) of @i pointer
+        @LOOP
+        D;JGT  // if the last address of the SCREEN's address space is not reached go to @LOOP
+        
+    @READ_KEYBOARD
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -3,7 +3,40 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
 
-// Multiplies R0 and R1 and stores the result in R2.
+// Multiplies the registers R0 and R1 and stores the result in the R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Given the fact that the exercise doesn't specifies if the values of the R0 and R1 RAM registers are costants or not,
+// I considered them as non constants for optimization reasons.
+
+    // R2 = 0
+    @R2
+    M=0
+    
+    // if (R1 == 0) go to @END;
+    @R1
+    D=M
+    @END
+    D;JEQ
+(LOOP)
+    // if (R0 == 0) go to @END;
+    // R0 = R0 - 1;
+    @R0
+    D=M
+    M=M-1
+    @END
+    D;JEQ
+
+    // R2 = R2 + R1
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    // go to @LOOP
+    @LOOP
+    0;JMP
+(END)
+    // go to @END
+    @END
+    0;JMP
